87d0cf0651d90d2e604df80b273e0fe4b636c806
suv-1312 <96910024+suv-1312@users.noreply.github.com>
2022-05-21 13:11:50 +0530

    Add files via upload

---
+++ 32 - Delete in a Binary Search Tree.cpp
@@ -0,0 +1,93 @@
+/*
+
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], key = 3
+Output: [5,4,6,2,null,null,7]
+Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,7], key = 0
+Output: [5,3,6,2,4,null,7]
+Explanation: The tree does not contain a node with value = 0.
+Example 3:
+
+Input: root = [], key = 0
+Output: []
+
+*/
+
+
+
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        if(root == NULL)
+            return NULL;
+        
+        if(root -> val > key)
+        {
+            root ->left = deleteNode(root->left,key);
+            return root;
+        }
+        else if(root -> val < key)
+        {
+            root ->right = deleteNode(root->right,key);
+            return root;
+        }
+        else
+        {
+            //IF NODE IS LEAF DIRECTLY  DELETE THE NODE AND RETURN
+
+            if(root ->left == NULL && root -> right == NULL)
+            {
+                delete root;
+                return NULL;
+            }
+            
+            //IF NODE HAS ONLY ONE CHILD
+
+            else if(root ->left == NULL)
+            {
+                TreeNode* temp = root -> right;
+                root -> right = NULL;
+                delete root;
+                return temp;
+            }
+            else if(root ->right == NULL)
+            {
+                TreeNode* temp = root -> left;
+                root -> left = NULL;
+                delete root;
+                return temp;
+            }
+
+            //IF NODE HAS BOTH THE CHILD
+            
+            else
+            {
+                TreeNode* mini = root ->right;
+                while(mini -> left != NULL)
+                    mini = mini -> left;
+                
+                int rightMin = mini -> val;
+                root -> val = rightMin;
+                root-> right = deleteNode(root -> right,rightMin);
+                return root;
+            }
+        }
+    }
+};
